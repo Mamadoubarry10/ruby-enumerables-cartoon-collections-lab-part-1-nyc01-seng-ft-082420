4a35a810a1fa7c099292568b56d234447f8b3732
mamadou alpha barry <mamadou.barry1105@gmail.com>
2020-08-16 19:52:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  array.each |x| do
+  array.each do |x|
     puts "Hello #{x}"
   end
     
